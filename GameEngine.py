0d8eacb9a1d3ad167df7e8eceddf4a6f01412140
kepan13 <oscarmelin1995@gmail.com>
2020-09-07 22:37:52 +0200

    now using python-chess

--- GameEngine.py
+++ GameEngine.py
@@ -1,186 +1,29 @@
+class MoveGenerator():
 
-class GameEngine:
-    def __init__(self):
-        self.board = [
-            ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
-            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
-            ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr']
-        ]
-        self.whitesTurn = True
-        self.moveLog = []
+    ranksToRows = {"1": 7, "2": 6, "3": 5,
+                   "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2,
+                   "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
 
-    # def getLegalMoves(self, piece):
-    #     legalMoves = []
-    #     if piece == 'br' or piece == 'wr':
-    #         legalMoves = rookLegalMoves()
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        # self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+    
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
 
-    def makeMove(self, move):
-        From = move[0]
-        To = move[1]
-        pieceMoved = self.board[From[0]][From[1]]
-        chessNotation = self.getChessNotation(From, To)
-        print(chessNotation)
-        self.board[From[0]][From[1]] = '--'
-        self.board[To[0]][To[1]] = pieceMoved
-        self.moveLog.append(chessNotation)
-        self.whitesTurn = not self.whitesTurn
 
-    def getChessNotation(self, From, To):
-        intToFile = {
-            0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'
-        }
-        intToRank = {
-            7: '1', 6: '2', 5: '3', 4: '4', 3: '5', 2: '6', 1: '7', 0: '8'
-        }
-        return intToFile[From[1]] + intToRank[From[0]] + intToFile[To[1]] + intToRank[To[0]]
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
 
-    def getAllLegalMoves(self):
-
-        legalMoves = []
-        # for r in range(8):
-        #     for c in range(8):
-        #         turn = self.board[r][c][0]
-        #         if (turn == 'w' and self.whitesTurn) or (turn == 'b' and not self.whitesTurn):
-        #             piece = self.board[r][c][1]
-        #         if piece == 'p':
-        #             self.getPawnMoves(r, c)
-        # turn = self.board[r][c][0]
-
-        for row in range(8):
-            for col in range(8):
-                turn = self.board[row][col][0]
-                if (turn == 'w' and self.whitesTurn) or (turn == 'b' and not self.whitesTurn):
-                    piece = self.board[row][col][1]
-                    if piece == 'p':  # pawn
-                        self.getPawnMoves(row, col, legalMoves)
-                    elif piece == 'r':  # rook
-                        self.getRookMoves(row, col, legalMoves)
-                    elif piece == 'b':  # bishop
-                        self.getBishopMoves(row, col, legalMoves)
-                    elif piece == 'q':
-                        self.getBishopMoves(row, col, legalMoves)
-                        self.getRookMoves(row, col, legalMoves)
-                    elif piece == 'k':
-                        self.getKingMoves(row, col, legalMoves)
-                    elif piece == 'n':
-                        self.getKnightMoves(row, col, legalMoves)
-        print(legalMoves)
-        return legalMoves
-
-    def getPawnMoves(self, r, c, moves):
-        # white
-        if self.board[r][c][0] == 'w' and self.whitesTurn:
-            if r-1 >= 0:
-                if self.board[r-1][c] == '--':
-                    moves.append(self.getChessNotation((r, c), (r-1, c)))
-            if r == 6 and self.board[r-2][c] == '--':
-                moves.append(self.getChessNotation((r, c), (r-2, c)))
-            if r-1 >= 0 and c+1 <= 7:
-                if self.board[r-1][c+1][0] == 'b':
-                    moves.append(
-                        self.getChessNotation((r, c), (r-1, c+1)))
-            if r-1 >= 0 and c-1 >= 0:
-                if self.board[r-1][c-1][0] == 'b':
-                    moves.append(
-                        self.getChessNotation((r, c), (r-1, c-1)))
-        # black
-        elif self.board[r][c][0] == 'b' and not self.whitesTurn:
-            if r+1 <= 7:
-                if self.board[r+1][c] == '--':
-                    moves.append(self.getChessNotation((r, c), (r+1, c)))
-            if r == 1 and self.board[r+2][c] == '--':
-                moves.append(self.getChessNotation((r, c), (r+2, c)))
-            if r+1 <= 7 and c+1 <= 7:
-                if self.board[r+1][c+1][0] == 'w':
-                    moves.append(
-                        self.getChessNotation((r, c), (r+1, c+1)))
-            if r+1 <= 7 and c-1 >= 0:
-                if self.board[r+1][c-1][0] == 'w':
-                    moves.append(
-                        self.getChessNotation((r, c), (r+1, c-1)))
-
-    def getRookMoves(self, r, c, moves):
-        enemyColor = 'b' if self.whitesTurn else 'w'
-        directions = ((-1, 0), (0, -1), (1, 0), (0, 1))
-        for d in directions:
-            for i in range(1, 8, 1):
-                row = r + d[0] * i
-                col = c + d[1] * i
-                if 0 <= row < 8 and 0 <= col < 8:
-                    endPiece = self.board[row][col]
-                    if endPiece == '--':
-                        moves.append(
-                            self.getChessNotation((r, c), (row, col)))
-                    elif endPiece[0] == enemyColor:
-                        moves.append(
-                            self.getChessNotation((r, c), (row, col)))
-                        break
-                    else:
-                        break
-                else:
-                    break
-
-    def getBishopMoves(self, r, c, moves):
-        enemyColor = 'b' if self.whitesTurn else 'w'
-        directions = ((-1, -1), (1, -1), (-1, 1), (1, 1))
-        for d in directions:
-            for i in range(1, 8, 1):
-                row = r + d[0] * i
-                col = c + d[1] * i
-                if 0 <= row < 8 and 0 <= col < 8:
-                    endPiece = self.board[row][col]
-                    if endPiece == '--':
-                        moves.append(
-                            self.getChessNotation((r, c), (row, col)))
-                    elif endPiece[0] == enemyColor:
-                        moves.append(
-                            self.getChessNotation((r, c), (row, col)))
-                    else:
-                        break
-                else:
-                    break
-
-    def getKnightMoves(self, r, c, moves):
-        allyColor = 'w' if self.whitesTurn else 'b'
-        knightMoves = (
-            (-2, 1),
-            (-1, 2),
-            (1, 2),
-            (2, 1),
-            (2, -1),
-            (1, -2),
-            (-1, -2),
-            (-2, -1)
-        )
-        for km in knightMoves:
-            row = r + km[0]
-            col = c + km[1]
-            if 0 <= row < 8 and 0 <= col < 8:
-                endPiece = self.board[row][col]
-                if endPiece[0] != allyColor:
-                    moves.append(self.getChessNotation((r, c), (row, col)))
-
-    def getKingMoves(self, r, c, moves):
-        allyColor = 'w' if self.whitesTurn else 'b'
-        directions = (
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-            (1, 0),
-            (1, -1),
-            (0, -1)
-        )
-        for i in range(8):
-            row = r + directions[i][0]
-            col = c + directions[i][1]
-            if 0 <= row < 8 and 0 <= col < 8:
-                endPiece = self.board[row][col]
-                if endPiece[0] != allyColor:
-                    moves.append(self.getChessNotation((r, c), (row, col)))
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- main.py
+++ main.py
@@ -1,95 +1,122 @@
+import chess
 import pygame
 import GameEngine
+
+def getChessNotation(startSq, endSq):
+
+    intToFile = {0:'a', 1:'b', 2:'c', 3:'d', 4: 'e',
+                5: 'f', 6:'g', 7:'h'}
+    intToRank = {0: '8',1: '7', 2: '6', 3: '5', 4: '4',
+                5: '3', 6: '2', 7: '1'}
+    return intToFile[startSq[1]] + intToRank[startSq[0]] + intToFile[endSq[1]] + intToRank[endSq[0]]
 
 WIDTH = HEIGHT = 800
 DIMENSION = 8
 SQUARE_SIZE = HEIGHT // DIMENSION
 IMAGES = {}
+moveLog = []
 
 
-def drawBoard(screen):
-    colors = [pygame.Color('navajowhite1'), pygame.Color('peru')]
-    for row in range(DIMENSION):
-        for col in range(DIMENSION):
-            # (r+c) % 2 == 0 --> white
-            color = colors[(row + col) % 2]
-            pygame.draw.rect(screen, color, (row*SQUARE_SIZE,
-                                             col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-
-
-def drawPieces(screen, board):
-    for r in range(DIMENSION):
-        for c in range(DIMENSION):
-            piece = board[r][c]
-            if piece != '--':
-                screen.blit(IMAGES[piece], pygame.Rect(
-                    c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-
+ugly_board = [
+                ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+                ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+                ['..', '..', '..', '..','..', '..', '..', '..'],
+                ['..', '..', '..', '..','..', '..', '..', '..'],
+                ['..', '..', '..', '..','..', '..', '..', '..'],
+                ['..', '..', '..', '..','..', '..', '..', '..'],
+                ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+                ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+            ]
 
 def loadImages():
-    pieces = ['br', 'bb', 'bn', 'bk', 'bq',
-              'bp', 'wr', 'wb', 'wn', 'wk', 'wq', 'wp']
+    pieces = ['bR', 'bB', 'bN', 'bK', 'bQ',
+              'bp', 'wR', 'wB', 'wN', 'wK', 'wQ', 'wp']
     for piece in pieces:
         IMAGES[piece] = pygame.image.load('pieces/' + piece + '.png')
         IMAGES[piece] = pygame.transform.scale(
             IMAGES[piece], (SQUARE_SIZE, SQUARE_SIZE))
 
 
-def updateBoard(screen, gameState):
-    drawBoard(screen)
-    drawPieces(screen, gameState.board)
+def drawBoard(screen):
+    colors = [pygame.Color('navajowhite1'), pygame.Color('peru')]
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            color = colors[(row + col) % 2]
+            pygame.draw.rect(screen, color, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-
-def display(board):
+def drawPieces(screen):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
-            print(board[r][c], end=" ")
-        print()
+            piece = ugly_board[r][c]
+            if piece != '..':
+                screen.blit(IMAGES[piece], pygame.Rect( c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+def updateBoard(screen):
+    drawBoard(screen)
+    drawPieces(screen)
+
+def make_move(startSq, endSq):
+    move = GameEngine.MoveGenerator(startSq, endSq, ugly_board)
+    # startRow = startSq[0]
+    # startCol = startSq[1]
+    # endRow = endSq[0]
+    # endCol = endSq[1]
+    ugly_board[move.startRow][move.startCol] = '..'
+    ugly_board[move.endRow][move.endCol] = move.pieceMoved
+
+    moveLog.append(move)
+
+def undoMove():
+    move = moveLog.pop()
+    ugly_board[move.startRow][move.startCol] = move.pieceMoved
+    ugly_board[move.endRow][move.endCol] = move.pieceCaptured
+    
 
 
-if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode([WIDTH, HEIGHT])
-    pygame.display.set_caption('Chess by Oscar')
-    loadImages()
-    playerClicks = []
-    selectedSquare = ()
-    gs = GameEngine.GameEngine()
-    legalMoves = gs.getAllLegalMoves()
-    moveMade = False
+pygame.init()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
-    while 1:
-
-        for e in pygame.event.get():
-
-            if e.type == pygame.QUIT:
-                pygame.quit()
-
-            elif e.type == pygame.MOUSEBUTTONDOWN:
-                coord = pygame.mouse.get_pos()
-                col = coord[0] // SQUARE_SIZE
-                row = coord[1] // SQUARE_SIZE
-                # If player clicks the same square twice
-                if selectedSquare == (row, col):
+board = chess.Board()
+print(board)
+loadImages()
+selectedSquare = ()
+clicks = []
+print(board.legal_moves)
+while 1:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            (x, y) = pygame.mouse.get_pos()
+            col = x // SQUARE_SIZE
+            row = y // SQUARE_SIZE
+            if selectedSquare == (row, col):
+                selectedSquare = ()
+                clicks = []
+            else:
+                selectedSquare = (row, col)
+                clicks.append(selectedSquare)
+            
+            if len(clicks) == 2:
+                res = getChessNotation(clicks[0], clicks[1])
+                move = chess.Move.from_uci(res)
+                
+                if move in board.legal_moves:
+                    make_move(clicks[0], clicks[1])
+                    board.push(move)
+                    clicks = []
                     selectedSquare = ()
-                    playerClicks = []
+                    print(board.legal_moves)
                 else:
-                    selectedSquare = (row, col)
-                    playerClicks.append(selectedSquare)
+                    clicks = [selectedSquare]
+        elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_z:
+                # undo move
+                if len(moveLog):
+                    board.pop()
+                    undoMove()
+                    print(board)
 
-                if len(playerClicks) == 2:
-                    move = gs.getChessNotation(
-                        (playerClicks[0]), (playerClicks[1]))
-                    if move in legalMoves:
-                        gs.makeMove(playerClicks)
-                        moveMade = True
-                        selectedSquare = ()
-                        playerClicks = []
-                    else:
-                        playerClicks = [selectedSquare]
-            if moveMade:
-                legalMoves = gs.getAllLegalMoves()
-                moveMade = False
 
-            updateBoard(screen, gs)
-            pygame.display.flip()
+    updateBoard(screen)
+    pygame.display.flip()
